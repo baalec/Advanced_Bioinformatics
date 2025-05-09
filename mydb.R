6bba3e811c167ce4f3ae9818955a401e820a27cd
Carlstedt92 <emilcarlstedt@hotmail.com>
2025-05-09 11:40:48 +0200

    Edited test database

--- mydb.R
+++ mydb.R
@@ -1,10 +1,19 @@
 library("DBI")
-?DBI
+library("RSQLite")
 
-# Create Database using RSQLite and DBI
+# Create and Connect Database using RSQLite and DBI
 mydb <- dbConnect(RSQLite::SQLite(), "my-db.sqlite")
 
+
+#Create table from dataframe mtcars
 dbWriteTable(mydb, "mtcars", mtcars)
+
+# List tables in mydb
 dbListTables(mydb)
 
+
+# Select top 5 entries in mtcars table
 dbGetQuery(mydb, "Select * FROM mtcars LIMIT 5")
+
+#Disconnect from Database
+dbDisconnect(mydb)
