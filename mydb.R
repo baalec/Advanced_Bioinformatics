213c8735c46ed2e10d5a1e7d03f737c58c17fe9b
baalec <alth0040@student.umu.se>
2025-05-28 23:57:40 +0200

    New db

--- mydb.R
+++ mydb.R
@@ -2,6 +2,8 @@ library("DBI")
 library("RSQLite")
 library("readxl")
 library(dbplyr)
+devtools::install("./Package/AllPackages")
+library(AllPackages)
 
 # Create and Connect Database using RSQLite and DBI
 mydb <- dbConnect(RSQLite::SQLite(), "my-db.sqlite")
@@ -31,14 +33,14 @@ dbWriteTable(mydb, "raw_data", sgRNA_MaGeCK_df)
 #dbGetQuery(mydb, "SELECT * from sgRNA_MaGeCK_data WHERE gene = 'TPX2'")
 # Connect and select sgRNA sequence, index, exon_position
 # and LFC score from dataset, save in df
-df <- dbGetQuery(mydb, "Select \"index\", sgrna, matched_exons, LFC FROM raw_data")
+df <- dbGetQuery(mydb, "Select \"index\", sgrna, matched_exons, avg_ctrl, LFC FROM raw_data")
 df <- na.omit(df)
 
 # Create model_df for storing relevant data
 positions <- 20
 bases <- c("A","C","G","T")
 column_names <- as.vector(sapply(1:positions, function(i) paste0(bases, i)))
-column_names <- c("index", column_names, "gc_content","matched_exons", "absLFC")
+column_names <- c("index", column_names, "gc_content","Exon_position", "RNAseq_expression", "absLFC")
 model_df <- data.frame(matrix(ncol = length(column_names),
                               nrow = 0))
 colnames(model_df) <- column_names
@@ -46,7 +48,6 @@ colnames(model_df) <- column_names
 number_seq <- nrow(df)
 
 # Calculating gc content and one hot encoding sequences, taking the abs of LFC
-source("Package/TranslateSGRNA/R/one_hot_encode.R")
 for (i in 1:number_seq) {
   sequence <- df[i, , drop = FALSE]
   if (nrow(sequence) > 0) {
@@ -59,8 +60,9 @@ for (i in 1:number_seq) {
     gc <- sum(base_counts[c("G","C")], na.rm = TRUE)
     gc_content <- gc/positions
     
-    row_df <- as.data.frame(t(c(i, encoded, gc_content, sequence$matched_exons,
-                                abs(sequence$LFC))), stringsAsFactors = FALSE)
+    row_df <- as.data.frame(t(c(i, encoded, gc_content, sequence$Exon_position,
+                                sequence$RNAseq_expression,abs(sequence$LFC))),
+                                stringsAsFactors = FALSE)
     colnames(row_df) <- column_names
     row_df <- as.data.frame(lapply(row_df, as.numeric))
     model_df <- rbind(model_df, row_df)
